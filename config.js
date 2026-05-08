cc6a7eaf2fad1ffb14afe7227ec2eb16d3ccbac8
DeDorpstafelEst <de.dorpstafel.est@gmail.com>
2026-05-08 21:59:42 +0200

    Fix: Change document.html to document.documentElement for font size setting

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ fetch('config.json')
         if (fontSizeParam && !isNaN(parseInt(fontSizeParam))) {
             CONFIG.fontSize = parseInt(fontSizeParam);
             document.body.style.fontSize = CONFIG.fontSize + 'px';
-            document.html.style.fontSize = CONFIG.fontSize + 'px'
+            document.documentElement.style.fontSize = CONFIG.fontSize + 'px';
         }
 
         const bgParam = getParam('bg');
